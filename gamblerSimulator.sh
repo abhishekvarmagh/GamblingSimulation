0c21ac264734e6701b7d6abeded13c2797d52adf
abhishekvarmagh <abhishektheverma@gmail.com>
2020-01-28 10:48:17 +0530

    ADDED : Display Win Or Loose

--- gamblerSimulator.sh
+++ gamblerSimulator.sh
@@ -4,3 +4,18 @@ echo "Gambling Simulation Problem"
 
 STAKE=100
 BET=1
+
+function bet()
+{
+	cash=$STAKE
+	if [ $(( RANDOM%2 )) -eq 1 ]
+	then
+		cash=$(( cash+BET ))
+		echo "Win"
+	else
+		cash=$(( cash-BET ))
+		echo "Loose"
+	fi
+}
+
+bet
