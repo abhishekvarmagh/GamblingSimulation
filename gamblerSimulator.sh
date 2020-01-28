a79e312e195d82f6a874a288b77b55006eb4948a
abhishekvarmagh <abhishektheverma@gmail.com>
2020-01-28 10:58:24 +0530

    ADDED : Resign For The Day Using Condition

--- gamblerSimulator.sh
+++ gamblerSimulator.sh
@@ -4,18 +4,24 @@ echo "Gambling Simulation Problem"
 
 STAKE=100
 BET=1
+PERCENT=50
+WON=$(( STAKE+(PERCENT*100/STAKE) ))
+LOOSE=$((STAKE-(PERCENT*100/STAKE)))
 
 function bet()
 {
-	cash=$STAKE
 	if [ $(( RANDOM%2 )) -eq 1 ]
 	then
 		cash=$(( cash+BET ))
-		echo "Win"
 	else
 		cash=$(( cash-BET ))
-		echo "Loose"
 	fi
 }
 
-bet
+cash=$STAKE
+while [[ $cash -ne $LOOSE && $cash -ne $WON ]]
+do
+	bet
+done
+
+echo $cash
